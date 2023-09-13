58602ba10c9b1184e6e2fb940f74d1183cde082d
roseygirlforra <fenix.k.97@outlook.com>
2023-09-13 18:57:03 +0200

    Properties for Villa and method that will display these

--- RealEstate/Villa.cs
+++ RealEstate/Villa.cs
@@ -8,5 +8,27 @@ namespace RealEstate
 {
     internal class Villa:Residential
     {
+
+        public int numOfRoomsInTotal { get; set; }
+        public int numOfGuestRooms { get; set; }
+        public int numOfBedRooms { get; set; }
+        public int numOfWc { get; set; }
+        public double villaSize { get; set; }
+
+
+        public override void DisplayDetails() {
+            Console.WriteLine($"Villa ID: {ID}");
+            Console.WriteLine($"Address: {Address.Street}, {Address.City}, {Address.ZipCode}, {Address.Country}");
+            Console.WriteLine($"Number of rooms: {numOfRoomsInTotal}");
+            Console.WriteLine($"Number of guest rooms: {numOfGuestRooms}");
+            Console.WriteLine($"Number of bed rooms: {numOfBedRooms}");
+            Console.WriteLine($"Number of WC: { numOfWc}");
+            Console.WriteLine($"Villa size: {villaSize}");
+        }
+
+
+
+
+
     }
 }
